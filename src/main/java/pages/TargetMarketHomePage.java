cafff65698a81e389dae09f99d3443a3ad008e7a
ibrahim <iboycl35@gmail.com>
2024-01-15 19:10:35 +0300

    First test related to Laptops category is implemented.

--- src/main/java/pages/TargetMarketHomePage.java
+++ src/main/java/pages/TargetMarketHomePage.java
@@ -1,15 +1,136 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
 
 public class TargetMarketHomePage extends BasePage {
 
 	@FindBy(xpath = "//*[@id='target-market']/div[1]/h5")
 	private WebElement welcomeText;
 
+	@FindBy(css = "#splide02-list > li")
+	private List<WebElement> productCategorySlide;
+
+	@FindBy(css = ".card-body")
+	private List<WebElement> productCards;
+
+	@FindBy(xpath = "//*[@id='navbar']/div/div/button[1]")
+	private WebElement logoutButton;
+
+	@FindBy(xpath = "//*[@id='navbar']/div/div/button[2]")
+	private WebElement cartButton;
+
+	@FindBy(xpath = "//*[@id='navbar']/div/div/button[2]/span")
+	private WebElement cartButtonProductNumber;
+
+	@FindBy(className = "modal-content")
+	private WebElement cartModal;
+
+	@FindBy(xpath = "//*[@class='modal-content']/div[3]")
+	private WebElement cartModalCloseButton;
+
+	@FindBy(css = ".list-group-item")
+	private List<WebElement> productsOnTheCart;
+
+	@FindBy(xpath = "//*[@class='modal-footer']/div/p")
+	private WebElement cartTotalPrice;
+
+	@FindBy(xpath = "//*[@class='modal-footer']/div/button")
+	private WebElement checkOutButton;
+
 	public String getWelcomeText() {
 		return welcomeText.getText();
+	}
+
+	public void  clickOnCategory(int index) {
+		productCategorySlide.get(index - 1).click();
+	}
+
+	public WebElement findProduct(String product) {
+		for (WebElement card : productCards) {
+			String productName =card.findElement(By.tagName("h5")).getText();
+			if (productName.equalsIgnoreCase(product)){
+				return card;
+			}
+		}
+		return null;
+	}
+
+	public String getProductName(String productName) {
+		return findProduct(productName)
+				.findElement(By.tagName("h5")).getText();
+	}
+
+	public String getProductPrice(String productName) {
+		return findProduct(productName)
+				.findElement(By.className("text-danger")).getText();
+	}
+
+	public String getDiscount(String productName) {
+		return findProduct(productName)
+				.findElement(By.xpath("//p[2]/span[2]")).getText();
+	}
+
+	public String getButtonText(String productName) {
+		return findProduct(productName)
+				.findElement(By.tagName("button")).getText();
+	}
+
+	public String getProductDescription(String productName) {
+		return findProduct(productName)
+				.findElement(By.className("card-text")).getText();
+	}
+
+	public void addToCart(String productName) {
+		findProduct(productName)
+				.findElement(By.tagName("button")).click();
+	}
+
+	public void clickOnCartButton() {
+		actions.moveToElement(cartButton).click().perform();
+	}
+
+	public void clickOnLogoutButton() {
+		actions.moveToElement(logoutButton).click().perform();
+	}
+
+	public String getProductNameOnTheCart(int index) {
+		return productsOnTheCart.get(index - 1)
+				.findElement(By.tagName("h3")).getText();
+	}
+
+	public String getPerProductPriceOnTheCart(int index) {
+		return productsOnTheCart.get(index - 1)
+				.findElement(By.xpath(".//div/div/p[1]")).getText();
+	}
+
+	public String getTotalProductPriceOnTheCart(int index) {
+		return productsOnTheCart.get(index - 1)
+				.findElement(By.xpath(".//div/div/p[2]")).getText();
+	}
+
+	public void clickOnIncreaseButtonOnCart(int index, int times) {
+		for (int i = 0; i < times; i++) {
+			productsOnTheCart.get(index - 1)
+					.findElement(By.xpath(".//button[contains(text(), '+')]"))
+					.click();
+		}
+	}
+
+	public void clickOnDecreaseButtonOnCart(int index, int times) {
+		for (int i = 0; i < times; i++) {
+			productsOnTheCart.get(index - 1)
+					.findElement(By.xpath(".//button[contains(text(), '-')]"))
+					.click();
+		}
+	}
+
+	public String getProductNumberOnTheCart(int index) {
+		return productsOnTheCart.get(index - 1)
+				.findElement(By.tagName("span")).getText();
 	}
 
 }
---
+++ src/test/java/orderProcessTests/laptopTests/TM_LA_01.java
@@ -0,0 +1,69 @@
+package orderProcessTests.laptopTests;
+
+import baseTest.Hooks;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+import pages.TargetMarketHomePage;
+
+// 1-) Navigate to ordering page.
+// 2-) Click on Laptops category.
+// 3-) Add MacBook Pro to cart.
+// 4-) Verify that button in MacBook Pro card is turned to "Added to Cart".
+// 5-) Click on cart button.
+// 6-) Verify that MacBook Pro is visible on the popup.
+// 7-) Verify that the price is the same on the popup.
+// 8-) Click on "+" button to increase the amount of item.
+// 9-) Verify that the amount is "2" and the total price is correct.
+public class TM_LA_01 extends Hooks {
+
+    SoftAssert softAssert = new SoftAssert();
+
+    @Test
+    void testLaptops() {
+
+        // 1-) Navigate to ordering page.
+        pages.getInarAcademyHomePage().clickOnTargetMarketLink();
+        pages.getTargetMarketLoginPage().login("standard_user", "secret_password");
+
+        TargetMarketHomePage homePage = pages.getTargetMarketHomePage();
+
+        // 2-) Click on Laptops category.
+        homePage.clickOnCategory(3);
+
+        // 3-) Add MacBook Pro to cart.
+        homePage.addToCart("MacBook Pro");
+
+        // 4-) Verify that button in MacBook Pro card is turned to "Added to Cart".
+        softAssert.assertEquals(homePage.getButtonText("MacBook Pro"),
+                "Added to Cart",
+                "Button text doesn't turn to 'Added to Cart'");
+
+        // 5-) Click on cart button.
+        homePage.clickOnCartButton();
+
+        // 6-) Verify that MacBook Pro is visible on the popup.
+        wait(2);
+        softAssert.assertEquals(homePage.getProductNameOnTheCart(1),
+                "MacBook Pro", "Product is not added to the cart"); ;
+
+        // 7-) Verify that the price is the same on the popup.
+        softAssert.assertTrue(homePage.getPerProductPriceOnTheCart(1)
+                        .contains(homePage.getProductPrice("MacBook Pro")),
+                "Product price is different on the cart");
+
+        // 8-) Click on "+" button twice to increase the amount of item to 3.
+        wait(2);
+        homePage.clickOnIncreaseButtonOnCart(1, 2);
+
+        // 9-) Verify that the amount is "3" and the total price is correct.
+        wait(2);
+        softAssert.assertEquals(homePage.getProductNumberOnTheCart(1),
+                "3", "Increase product amount button on the cart doesn't work");
+
+        int totalPrice = Integer.parseInt(homePage.getProductPrice("MacBook Pro").substring(1)) * 3;
+        softAssert.assertTrue(homePage.getTotalProductPriceOnTheCart(1).contains("$" + totalPrice) ,
+                "Total price is wrong on the cart");
+
+        softAssert.assertAll("Laptops test 1 cannot be completed:");
+    }
+}
